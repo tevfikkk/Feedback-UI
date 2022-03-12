f6328749d22a4ce4a936f5811c42995c4ee62c2e
[Tevfik] <tevfikhoglu@gmail.com>
2022-03-12 16:04:52 +0300

    Refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import React, { useState } from 'react'
+import React from 'react'
 import Header from './components/Header'
 // import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
 // import FeedbackStats from './components/FeedbackStats'
 // import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import Feedback from './components/Feedback'
 
 const App = () => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const deleteFeedback = id => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter(item => item.id !== id))
-    }
-  }
-
-  const addFeedback = newFeedback => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
   return (
     <Router>
       <Header text='Feedback UI' bgColor='red' textColor='blue' />
@@ -34,11 +19,7 @@ const App = () => {
             path='/'
             element={
               <>
-                <Feedback
-                  addFeedback={addFeedback}
-                  deleteFeedback={deleteFeedback}
-                  feedback={feedback}
-                />
+                <Feedback />
               </>
             }
           />
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
 import FeedbackForm from './FeedbackForm'
 import FeedbackList from './FeedbackList'
 import FeedbackStats from './FeedbackStats'
+import { useState } from 'react'
+import FeedbackData from '../data/FeedbackData'
 
-const Feedback = ({ addFeedback, feedback, deleteFeedback }) => {
+const Feedback = () => {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = id => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+  }
+
+  const addFeedback = newFeedback => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
   return (
     <>
       <FeedbackForm handleAdd={addFeedback} />
